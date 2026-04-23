daca81958b5b87d9986c4796cc3b4a3b1658de7f
Parth080 <parth.abhang@flamapp.com>
2026-04-23 13:02:19 +0530

    commit

--- poll_schedule_config.py
+++ poll_schedule_config.py
@@ -11,9 +11,8 @@ POLL_TIMEZONE = "Asia/Kolkata"
 # Post the food poll at these local times. Empty list = no cron (only /startpoll).
 # Example: lunch + dinner
 POLL_SCHEDULE_IST = [
-    "11:23",
-    "11:24",
-    "11:25",
+    "14:30",
+    "20:30"
 ]
 
 # APScheduler day_of_week. None = every day.
